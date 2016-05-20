9ca965a90c531b609eccf946d34fa80a98f424e7
Eli Bermudez <bermudez.86@live.com.mx>
2016-05-19 22:05:44 -0500

    Validaciones formulario  JS

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,11 @@
 document.formulario.addEventListener('submit', validarFormulario);
 
-function validarFormulario(evObject) {
-	 alert("Integrado")
+function validarFormulario(evObject){
+ evObject.preventDefault();//Evita que el formulario se envie
+ var nombre = document.getElementById('nombre').value;
+ var correo = document.getElementById('correo').value;
+ var telefono = document.getElementById('telefono').value;
+ if(nombre=="")alert("Nombre esta vacio")
+ else if(correo=="")alert("El Correo esta vacio")
+
 }
